56216bb6ee51661490d09ad9ed0bda013e26db3a
Scott <scott@scotts-mbp.lan>
2023-04-11 13:57:06 -0700

    api

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const product = await prisma.product.findMany({
+        where: {
+          id: req?.query?.id?.toString(),
+        },
+        include: {
+          category: true,
+        },
+      });
+      return res.status(200).json(product);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+}
